373445714c52219a478b5e32a7debc7b34f1e076
Narayan <laxminarayanreddy432@gmail.com>
2023-01-05 21:01:40 +0530

    cli-init

---
+++ bin/extension_enabler.dart
@@ -0,0 +1,5 @@
+import 'package:extension_enabler/extension_enabler.dart' as extension_enabler;
+
+void main(List<String> arguments) {
+  print('Hello world: ${extension_enabler.calculate()}!');
+}
---
+++ test/extension_enabler_test.dart
@@ -0,0 +1,8 @@
+import 'package:extension_enabler/extension_enabler.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
