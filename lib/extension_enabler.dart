08dc203628cc5a89eb167afd6751050d5320b3a4
Narayan <laxminarayanreddy432@gmail.com>
2023-01-06 22:29:54 +0530

    testing-packages

--- lib/extension_enabler.dart
+++ lib/extension_enabler.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
-
+import 'package:html/parser.dart';
+import 'package:path/path.dart' as path;
 import 'package:args/args.dart';
 
 void mainx(List<String> arguments) async {
@@ -9,12 +10,27 @@ void mainx(List<String> arguments) async {
   final results = parcer.parse(arguments);
   if (results["enable"] == true) {
     // stdout.write("hello");
-    var myFile = File('file.txt');
-    myFile.writeAsStringSync('Hello World');
-    await myFile.create(recursive: true);
+    // var myFile = File('file.txt');
+    // myFile.writeAsStringSync('Hello World');
+    // await myFile.create(recursive: true);
     // var result =
     //     await Process.run('powershell.exe', ['-command', "dart --version"]);
     // stdout.write(result.stdout);
+    var filePath = path.join(Directory.current.path, "web", 'index.html');
+    File file = File(filePath);
+    var fileContent = file.readAsStringSync();
 
+    var document = parse(fileContent);
+    // var script = document.querySelector('script');
+    // script!.attributes['src'] = 'main.dart.js';
+
+    // var str = document.getElementsByTagName('script')[0].outerHtml;
+
+    document.getElementsByTagName('head')[0].attributes['height'] = "100px";
+    stdout.write(document.toString());
+
+    file.writeAsStringSync(document.outerHtml);
+    fileContent = file.readAsStringSync();
+    stdout.write(fileContent);
   }
 }
